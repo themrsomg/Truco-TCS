cca7c60326d54b68167de7f7b00e9059454da822
themrsomg <themrsomg117@gmail.com>
2025-10-07 11:56:03 -0600

    refactor: Se cambiaron ajustes

--- TrucoPrueba1/baseDatosTruco.Designer.cs
+++ TrucoPrueba1/baseDatosTruco.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\genae\OneDrive\Documentos\111-UV\Quinto semestre\Tecnologías para la Construcción de Software\Actividades\C#\TrucoPrueba1\TrucoPrueba1\baseDatosTruco.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\themr\source\repos\themrsomg\Truco-TCS\TrucoPrueba1\baseDatosTruco.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
