c817e83025a0c1a37235d44af9ee8f027313d0cf
themrsomg <themrsomg117@gmail.com>
2025-10-22 12:01:46 -0600

    feat: Se añadió un easter egg

--- TrucoPrueba1/Views/CreditsPage.xaml.cs
+++ TrucoPrueba1/Views/CreditsPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.IO;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -29,6 +32,33 @@ namespace TrucoPrueba1
         private void ClickBack(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new SettingsPage());
+        }
+        private void ClickEgg(object sender, RoutedEventArgs e)
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string fullPath = System.IO.Path.Combine(currentDirectory, "Resources", "NobodyBeatsTheHammer.exe");
+
+            try
+            {
+                if (!File.Exists(fullPath))
+                {
+                    MessageBox.Show($"Error de Archivo: El ejecutable no fue encontrado en la ruta: {fullPath}", "Error de Ejecución", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                Process.Start(fullPath);
+                if (this.NavigationService != null)
+                {
+                    this.NavigationService.Navigate(new SettingsPage());
+                }
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                MessageBox.Show($"Error: No se pudo ejecutar el programa.Detalle: {ex.Message}", "Error Crítico", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error general al ejecutar: {ex.Message}", "Error de Ejecución", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
