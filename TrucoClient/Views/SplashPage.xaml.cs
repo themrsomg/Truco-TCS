d36ddabdf1100c17a2efddd89c9575a41e19278e
themrsomg <themrsomg117@gmail.com>
2025-10-27 18:11:09 -0600

    feat: Se añadieron nuevos elementos al proyecto

---
+++ TrucoClient/Views/SplashPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrucoClient.Views
+{
+    /// <summary>
+    /// Lógica de interacción para SplashPage.xaml
+    /// </summary>
+    public partial class SplashPage : Page
+    {
+        public SplashPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
